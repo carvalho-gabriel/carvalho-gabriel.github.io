5eb5de6fd0f3b32004f83d774e1ca33ea4fe1a1b
Gabriel Carvalho <46932805+carvalho-gabriel@users.noreply.github.com>
2023-08-15 19:09:57 -0300

    geo

---
+++ script.js
@@ -0,0 +1,13 @@
+let h2 = document.querySelector('h2');
+
+function sucess(pos){
+  console.log(pos, pos.coords.latitude,pos.coords.longitude);
+  h2.textContent = `Latitude:${pos.coords.latitude} - Longitude:${pos.coords.longitude}`;
+}
+
+function error(err){
+  console.log(err);
+}
+
+var watcherID = navigator.geolocation.watchPosition(sucess, error, {
+  enableHighAccuracy: true, maximumAge: 60000});
